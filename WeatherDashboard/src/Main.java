7234c2f5fb82ffd9e4ac9f481ecca420067737f6
NikhilKatkuri <knikhil07k@gmail.com>
2025-06-04 11:39:18 +0530

    pushed from git-lite-cli

--- WeatherDashboard/src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-import App_ui.AppMain;
-public class Main {
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(AppMain::new);
-    }
-}
--- WeatherDashboard/src/Models/WeatherData.java
+++
@@ -1,5 +0,0 @@
-package Models;
-
-public record WeatherData(float temp, float feelsLike, float tempMin, float tempMax, int pressure, int humidity,
-                          String cityName, long sunrise, long sunset, long currentTime) {
-}
